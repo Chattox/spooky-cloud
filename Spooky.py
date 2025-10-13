d4398c113ad1264a408ae6816378633317522ea3
Chattox <conradlineker@gmail.com>
2025-10-13 11:09:33 +0100

    feat: vary background colour
    also change colour values from RGB to HSV

--- Spooky.py
+++ Spooky.py
@@ -1,6 +1,7 @@
 import random
 import time
 import plasma
+from pimoroni import RGBLED
 import colours
 
 class Spooky:
@@ -9,6 +10,7 @@ class Spooky:
     """
     
     def __init__(self, num_leds):
+        self.plasma_led = RGBLED()
         self.NUM_LEDS = num_leds
         self.strip = plasma.WS2812(self.NUM_LEDS, color_order=plasma.COLOR_ORDER_RGB)
         self.bg_colours = [colours.BLANK, colours.RED, colours.GREEN, colours.BLUE, colours.ORANGE, colours.TEAL]
@@ -33,9 +35,8 @@ class Spooky:
         self.next_flash_delay_max = 75
         # Range for brightness variance of background lighting
         self.b_variance_min = 0
-        self.b_variance_max = 10
-        
-        
+        self.b_variance_max = 50
+             
     def startup(self):
         """
             Start process for lights
@@ -43,17 +44,47 @@ class Spooky:
         
         self.strip.start()
         self.clear()
+        self.plasma_led.set_rgb(0, 255, 0)
+        
+    def convert_hsv(self, hsv):
+        """
+            The plasma library requires HSV values to be weird because pimoroni are
+            weird little guys. Decided I'd rather keep the constants to real HSV
+            values and just convert them as needed
+            
+            Args:
+                hsv: tuple of real HSV value
+                
+            Returns:
+                tuple of HSV value converted to pimoroni requirements
+        """
+        
+        h, s, v = hsv
+        new_h = h / 360
+        new_s = s / 100
+        new_v = v / 100
+        
+        return (new_h, new_s, new_v)
+        
+    def get_cur_bg(self):
+        """
+            Returns current background colour
+            
+            Returns:
+                Tuple of current bg colour as HSV
+        """
+        return self.bg_colours[self.current_bg_i]
             
     def set_all(self, colour):
         """
             Set all LEDs in the strip to the same colour at once
             
             Args:
-                colour: Tuple containing RGB value
+                colour: Tuple containing HSV value
         """
-        r, g, b = colour
+        h, s, v = self.convert_hsv(colour)
         for i in range(self.NUM_LEDS):
-            self.strip.set_rgb(i, r, g, b)
+            self.strip.set_hsv(i, h, s, v)
             
     def clear(self):
         """
@@ -70,38 +101,49 @@ class Spooky:
         else:
             self.current_bg_i += 1
             
-        colour = self.bg_colours[self.current_bg_i]
+        colour = self.get_cur_bg()
         
         # Vary the brightness on each LED a little to make it look more natural
         for i in range(self.NUM_LEDS):
-            variance = random.randint(0, 10)
-            r, g, b = colour
-            r -= variance
-            g -= variance
-            b -= variance
-            
-            self.strip.set_rgb(i, r, g, b)
+            v_col = self.convert_hsv(self.vary_brightness(colour))
+            self.strip.set_hsv(i, *v_col)
             
     def vary_brightness(self, col):
         """
-            Varies the brightness of an RGB value by a random amount within the
+            Varies the brightness of an HSV value by a random amount within the
             specified min and max
             
             Args:
-                col: Tuple of RGB value
+                col: Tuple of HSV value
                 
             Returns:
-                Tuple of new RGB value
+                Tuple of new HSV value
         """
+        # If colour is blank, skip
+        if col == (0.0, 0.0, 0.0):
+            return col
+        
         # Get variance
         v = random.randint(self.b_variance_min, self.b_variance_max)
         # Decide if positive or negative variance
         i = random.randint(0,1)
         if i == 0:
+            # We don't want the decrease in brightness to be
+            # as severe as the increase
+            v = v * 0.75
             v = -v
-        
-        for i in range(len(col)):
             
+        varied_col = list(col)
+        
+        brightness = varied_col[2] + v
+        if brightness > 100.0:
+            brightness = 100.0
+        elif brightness < 0.0:
+            brightness = 0.0
+        
+        varied_col[2] = brightness
+            
+        return tuple(varied_col)
             
             
     def toggle_lightning(self, button_a):
@@ -126,6 +168,6 @@ class Spooky:
             flash_colour = (f_b, f_b, f_b)
             self.set_all(flash_colour)
             time.sleep_ms(random.randint(self.flash_duration_min, self.flash_duration_max))
-            self.set_all(self.bg_colours[self.current_bg_i])
+            self.set_all(self.get_cur_bg())
             time.sleep_ms(random.randint(self.next_flash_delay_min, self.next_flash_delay_max))
         
